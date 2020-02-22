211ce1bf7190be540623441bd7621e0b0db61956
Piotr Hyriak <piotr1hyriak@gmail.com>
2020-02-22 12:05:20 +0100

    secound message, connected to database, change username and pasword in application propperties to connect, properties should be create if the databe is creted or update if we wont to update

--- src/test/java/pl/sda/ticketing_software_sda_gp/TicketingSoftwareSdaGpApplicationTests.java
+++ src/test/java/pl/sda/ticketing_software_sda_gp/TicketingSoftwareSdaGpApplicationTests.java
@@ -3,8 +3,7 @@ package pl.sda.ticketing_software_sda_gp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import pl.sda.ticketing_software_sda_gp.tickets.repository.ConversationElementRepository;
-import pl.sda.ticketing_software_sda_gp.tickets.repository.ConversationRepository;
+
 
 @SpringBootTest
 class TicketingSoftwareSdaGpApplicationTests {
