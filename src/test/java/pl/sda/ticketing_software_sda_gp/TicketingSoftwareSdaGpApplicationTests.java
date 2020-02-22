05e346d4587a18d3c118c7c20a03b74f196fab7d
Wookie <53392926+LukaszKrzysko@users.noreply.github.com>
2020-02-22 11:26:41 +0100

    first commit

---
+++ src/test/java/pl/sda/ticketing_software_sda_gp/TicketingSoftwareSdaGpApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.ticketing_software_sda_gp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.sda.ticketing_software_sda_gp.tickets.repository.ConversationElementRepository;
+import pl.sda.ticketing_software_sda_gp.tickets.repository.ConversationRepository;
+
+@SpringBootTest
+class TicketingSoftwareSdaGpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
